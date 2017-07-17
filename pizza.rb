dd4fec0e9ecb223d212937ae18ef6b33a5279fee
ANITA KING <ADKING>
2017-07-17 13:49:18 -0400

    failure is acceptable cost not working

--- pizza.rb
+++ pizza.rb
@@ -24,14 +24,45 @@ def number_of_pizzas()
 	number_of_pizzas.to_i
 end
 
-total_cost = number_of_pizzas *	10
-	p "#{total_cost} is your order total."
+def crust_cost()
+	counter = 0
+	if pizza_crust[thin]
+		counter = counter + 2
+	elsif pizza_crust[pan]
+		counter = counter +3
+	else
+		counter + 0
+end
 
+def pizza_size_cost()
+	if pizza_size[small]
+		counter = counter + 10 # still need times nmbr of pizzas
+	elsif pizza_size[medium] #still need times nmbr of pizzas
+		counter = counter + 15
+	else pizza_size[large]
+		counter = counter + 20 #still need times numbr of pizzas
+end
+
+def extra_cost()
+	if special_toppings[extra cheese] or special_toppings[double meat] 
+		counter = counter + 2
+	end
+end
+	
 def make_pizza()
 		pizza_crust + " " + pizza_size + " " + meats + " " + veggies + " " + special_toppings
+end
+
+number_of_pizzas
 end
 
 make_pizza
 	number_of_pizzas.times do
 	p "Your pizza order is #{make_pizza}"
 	end
+end
+def total_cost()
+	total_cost = "#{pizza_size_cost}" + "#{extra_cost}" + "#{crust_cost}"
+	p "#{total_cost} is your order total."
+end
+end
